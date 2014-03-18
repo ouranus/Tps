956cfbb5c45f0ebe55cf594aff89fec057fb33aa
ouranus <gui.afonso2010@gmail.com>
2014-03-18 00:20:56 -0300

    fila complete and stack

---
+++ Stack.cpp
@@ -0,0 +1,18 @@
+/*
+ * Stack.cpp
+ *
+ *  Created on: 17/03/2014
+ *      Author: guigui
+ */
+
+#include "Stack.h"
+
+Stack::Stack() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Stack::~Stack() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Stack.h
@@ -0,0 +1,17 @@
+/*
+ * Stack.h
+ *
+ *  Created on: 17/03/2014
+ *      Author: guigui
+ */
+
+#ifndef STACK_H_
+#define STACK_H_
+
+class Stack {
+public:
+	Stack();
+	virtual ~Stack();
+};
+
+#endif /* STACK_H_ */
